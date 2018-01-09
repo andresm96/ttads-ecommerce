b210a95853769685a7026464a811695f721a9373
andresm96 <andres-malvestiti@hotmail.com>
2018-01-09 20:45:56 -0300

    volviendo atras

--- Frontend/src/app/models/subcategory.ts
+++ Frontend/src/app/models/subcategory.ts
@@ -1,7 +1,9 @@
 import { Product } from "./product";
+import { Category } from "./category";
 
 export class Subcategory {
     id: string;
     name: string;
+    category: String;
     products: Product[];
 }
