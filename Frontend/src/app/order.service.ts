068514b388fa951e61f53b957e68c532cb7245a2
Martin Bianchi <tinchola7@gmail.com>
2018-02-05 10:57:38 -0300

    Add update to service order

--- Frontend/src/app/order.service.ts
+++ Frontend/src/app/order.service.ts
@@ -31,6 +31,15 @@ export class OrderService {
     );
   }
 
+  updateOrder (order: Order | number): Observable<Order> {
+    const id = typeof order === 'number' ? order : order._id;
+    const url = `${this.orderUrl+ "update"}/${id}`;
+
+    return this.http.put(url, order, httpOptions).pipe(
+      catchError(this.handleError<any>('updateOrder'))
+    );
+  }
+
    /**
    * Handle Http operation that failed.
    * Let the app continue.
